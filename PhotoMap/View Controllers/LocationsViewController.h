2ea8f31c7677ef4b1d1ea4e92a77040fff48511b
Airei Fukuzawa <eyeray@fb.com>
2022-06-29 10:42:24 -0700

    first commit

---
+++ PhotoMap/View Controllers/LocationsViewController.h
@@ -0,0 +1,21 @@
+//
+//  LocationsViewController.h
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@protocol LocationsViewControllerDelegate;
+
+@interface LocationsViewController: UIViewController
+@property (strong, nonatomic) UIImage *image;
+@property (weak, nonatomic) id<LocationsViewControllerDelegate> delegate;
+
+@end
+@protocol LocationsViewControllerDelegate
+
+- (void)locationsViewController:(LocationsViewController *)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude;
+
+@end
---
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -0,0 +1,103 @@
+//
+//  PhotoMapViewController.m
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "PhotoMapViewController.h"
+#import "LocationsViewController.h"
+
+@interface PhotoMapViewController () <UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) UIImage *selectedImage;
+
+@end
+
+@implementation PhotoMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+    
+    self.mapView.delegate = self;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)tapCamera:(id)sender {
+    // Image view controller
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+    [self performSegueWithIdentifier:@"tagSegue" sender:NULL];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.selectedImage = originalImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void) locationsViewController:(LocationsViewController *)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude {
+    [self.navigationController popViewControllerAnimated:YES ];
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    // Drop pin
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = @"Picture!";
+    [self.mapView addAnnotation:annotation];
+
+}
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+     MKPinAnnotationView *annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+     if (annotationView == nil) {
+         annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+         annotationView.canShowCallout = true;
+         annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+     }
+
+     UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+     imageView.image = [UIImage imageNamed:@"camera-icon"];
+
+     return annotationView;
+ }
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    if([segue.identifier isEqual: @"tagSegue"]) {
+        LocationsViewController *locationsVC = [segue destinationViewController];
+        locationsVC.image = self.selectedImage;
+        locationsVC.delegate = self;
+    }
+}
+
+
+@end
